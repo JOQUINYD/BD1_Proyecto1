dd1b625053419fe555da94cb17e64239915c0fc6
JOQUINYD <54180615+JOQUINYD@users.noreply.github.com>
2020-05-08 19:55:55 -0600

    ?
    No se que cambio

--- TiendaRepuestosAutoBD/BD1_Project1/dbo/Cliente.sql
+++ TiendaRepuestosAutoBD/BD1_Project1/dbo/Cliente.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Cliente]
 (
-	[IdCliente] INT NOT NULL PRIMARY KEY, 
+	[IdCliente] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Ciudad] NVARCHAR(50) NOT NULL, 
     [Direccion] NVARCHAR(50) NULL
 )
